55fe4faf06cf60573569b4b784d374bfc28ba70e
Liubomyr <lubchikpenkov@gmail.com>
2018-06-20 23:08:15 +0300

    set port for running app

--- server/server.ts
+++ server/server.ts
@@ -4,9 +4,11 @@ import router from './router'
 
 const app: express.Application = express();
 
+app.set('port', (process.env.PORT || 3000));
+
 app.use(express.static(path.resolve(__dirname, '../dist')));
 app.use('/', router);
 
-app.listen(3000, ()=>{
+app.listen(app.get('port'), ()=>{
     console.log('running on 3000 port');
 });
